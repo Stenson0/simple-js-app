4a0d7747a9a25db21d8cad0487360f287a659e88
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2024-04-16 11:12:06 -0500

    changed height to numbers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: 'Pikachu', height: '0.4m', type: ['electric']},
-    {name: 'Charizard', height: '1.7m', type: ['fire', 'flying']},
-    {name: 'Squirtle', height: '0.5m', type: ['water']},
+    {name: 'Pikachu', height: 0.4, type: ['electric']},
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+    {name: 'Squirtle', height: 0.5, type: ['water']},
 ];
