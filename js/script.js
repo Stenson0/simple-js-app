411f145c3908f7d18ae5d0eca9b9c01f41fef0a8
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2024-04-16 11:11:16 -0500

    added arrays to pokemon type

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: 'Pikachu', height: '0.4m', type: 'electric'},
+    {name: 'Pikachu', height: '0.4m', type: ['electric']},
     {name: 'Charizard', height: '1.7m', type: ['fire', 'flying']},
-    {name: 'Squirtle', height: '0.5m', type: 'water'},
+    {name: 'Squirtle', height: '0.5m', type: ['water']},
 ];
