eaa1f34ee659f63b216fd20c7a0c10402dd48fd1
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2024-04-18 15:55:46 -0500

    wrapped pokemonList in an IIFE

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-let pokemonList = [
-    {name: 'Pikachu', height: 0.4, type: ['electric']},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Squirtle', height: 0.5, type: ['water']},
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Pikachu', height: 0.4, type: ['electric']},
+        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+        {name: 'Squirtle', height: 0.5, type: ['water']},
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
 
 pokemonList.forEach(pokemon => {
     document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
